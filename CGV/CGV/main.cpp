fec20ca7c540e1a7dabb2c7735127ba50999cef5
Elias <elias_piovani@hotmail.com>
2018-06-10 15:10:00 +0200

    Link to libraries corrected

--- CGV/CGV/main.cpp
+++ CGV/CGV/main.cpp
@@ -160,7 +160,7 @@ int main( void )
     glBindVertexArray(VertexArrayID);
     
     // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders( "shaders/TransformVertexShader.vertexshader", "shaders/TextureFragmentShader.fragmentshader" );
+    GLuint programID = LoadShaders( "./shaders/TransformVertexShader.vertexshader", "./shaders/TextureFragmentShader.fragmentshader" );
     
     // Get a handle for our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
